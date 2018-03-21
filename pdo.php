9477f00c8ae9308117e7f0606f334282e2308fba
user <user@is2401a.univ-savoie.fr>
2018-03-21 09:56:07 +0100

    ignore pdo

--- pdo.php
+++ pdo.php
@@ -3,7 +3,7 @@ $PARAM_hote='localhost'; // le chemin vers le serveur
 $PARAM_port='80';
 $PARAM_nom_bd='click_to_call'; // le nom de votre base de données
 $PARAM_utilisateur='root'; // nom d'utilisateur pour se connecter
-$PARAM_mot_passe=''; // mot de passe de l'utilisateur pour se connecter
+$PARAM_mot_passe='root'; // mot de passe de l'utilisateur pour se connecter
 /*try
 {
 	$connexion = new PDO('mysql:host='.$PARAM_hote.';dbname='.$PARAM_nom_bd, $PARAM_utilisateur, $PARAM_mot_passe);
