c7255551a743cfbb64ff63cae3d53736d7ffa1e4
user <user@is2401a.univ-savoie.fr>
2018-03-21 11:41:39 +0100

    Indentation

--- historique.php
+++ historique.php
@@ -1,62 +1,60 @@
-<?php 
-session_start(); 
-     
-require'pdo.php';
+<?php
+session_start();
+
+require 'pdo.php';
 // Connexion à la base de données
-$bdd = new PDO('mysql:host='.$PARAM_hote.';dbname='.$PARAM_nom_bd.';charset=utf8',$PARAM_utilisateur , $PARAM_mot_passe);
- 
+$bdd = new PDO('mysql:host=' . $PARAM_hote . ';dbname=' . $PARAM_nom_bd . ';charset=utf8', $PARAM_utilisateur, $PARAM_mot_passe);
+
 // Condition pour afficher la page : être technicien Sinon redirection vers la page de connexion
-if(!empty($_SESSION['login'])){
-  
-  if($_SESSION["rang"]==0){
-      header('Location: client.php');
-  }
+if (!empty($_SESSION['login'])) {
+
+    if ($_SESSION["rang"] == 0) {
+        header('Location: client.php');
+    }
+} else {
+    header('Location: login.php');
 }
-else{
-  header('Location: login.php');
-}
-include("include/head.php");
+include "include/head.php";
 ?>
 
-<?php   
+<?php
 echo $_POST["id"];
-					  $client_ctc='SELECT * FROM ctc_request WHERE user_id='.$_POST["id"].' AND (etat=1 or etat=2)' ;
-					  $client_ctc_request = $bdd->query($client_ctc);
-					  
+$client_ctc = 'SELECT * FROM ctc_request WHERE user_id=' . $_POST["id"] . ' AND (etat=1 or etat=2)';
+$client_ctc_request = $bdd->query($client_ctc);
 
-					  $client_info='SELECT * FROM users WHERE id='.$_POST["id"];
-					  $client_private_info = $bdd->query($client_info);
-					  $array_client_private_info = $client_private_info->fetch();
+$client_info = 'SELECT * FROM users WHERE id=' . $_POST["id"];
+$client_private_info = $bdd->query($client_info);
+$array_client_private_info = $client_private_info->fetch();
 
-				//Affichage du tableau des requetes click to call
-				echo"<div class='card mb-3'>
+//Affichage du tableau des requetes click to call
+echo "<div class='card mb-3'>
 				<div class='card-header'>
-				<h3 style='text-align:center; color:black;'>Tableau des Requêtes du client nommé ".$array_client_private_info['nom']."</h1>
+				<h3 style='text-align:center; color:black;'>Tableau des Requêtes du client nommé " . $array_client_private_info['nom'] . "</h1>
 				</div>
 				<div class='card-body'>
-				  <div class='table-responsive'>
+					<div class='table-responsive'>
 					<table class='table table-bordered' id='dataTable' width='100%' cellspacing='0'  style='text-align:center;'>
-					  <thead>
+						<thead>
 						<tr>
-						  <th>ID requête</th>
-						  <th>ID client</th>
-						  <th>Date de requête</th>
-						  <th>Etat de la requête</th>
-						  <th>Commentaire</th>
+							<th>ID requête</th>
+							<th>ID client</th>
+							<th>Date de requête</th>
+							<th>Etat de la requête</th>
+							<th>Commentaire</th>
 
 						</tr>
-					  </thead>
-					  <tbody>";
-					  while($array_client_ctc_request = $client_ctc_request->fetch()){
-					  ?>
-					  <td><?php echo $array_client_ctc_request["id"] ?></td>
-					  <td><?php echo $array_client_ctc_request["user_id"] ?></td>
-					  <td><?php echo $array_client_ctc_request["dates"] ?></td>
-					  <td><?php echo $array_client_ctc_request["etat"] ?></td>
-					  <td><?php echo $array_client_ctc_request["Commentaire"] ?></td>
-					  </tr>
-					  </tbody>
-					  <?php }  ?> 
+						</thead>
+						<tbody>";
+while ($array_client_ctc_request = $client_ctc_request->fetch()) {
+    ?>
+						<td><?php echo $array_client_ctc_request["id"] ?></td>
+						<td><?php echo $array_client_ctc_request["user_id"] ?></td>
+						<td><?php echo $array_client_ctc_request["dates"] ?></td>
+						<td><?php echo $array_client_ctc_request["etat"] ?></td>
+						<td><?php echo $array_client_ctc_request["Commentaire"] ?></td>
+						</tr>
+						</tbody>
+						<?php }?>
 					</table>
 				</div>
 				</div>
@@ -64,10 +62,10 @@ echo $_POST["id"];
 				<div style="width:50%;margin-left:25%;">
 				<a href="index.php"><button type=button class="btn btn-primary">Revenir en arrière</button></a>
 				</div>
-		
-		
-<?php 
-include("include/foot.php");
+
+
+<?php
+include "include/foot.php";
 ?>
  </div>
 </body>
--- index.php
+++ index.php
@@ -1,29 +1,28 @@
-<?php 
-session_start(); 
-     
-require'pdo.php';
+<?php
+session_start();
+
+require 'pdo.php';
 // Connexion à la base de données
-$bdd = new PDO('mysql:host='.$PARAM_hote.';dbname='.$PARAM_nom_bd.';charset=utf8',$PARAM_utilisateur , $PARAM_mot_passe);
- 
+$bdd = new PDO('mysql:host=' . $PARAM_hote . ';dbname=' . $PARAM_nom_bd . ';charset=utf8', $PARAM_utilisateur, $PARAM_mot_passe);
+
 // Condition pour afficher la page : être technicien Sinon redirection vers la page de connexion
-if(!empty($_SESSION['login'])){
-  
-  if($_SESSION["rang"]==0){
-      header('Location: client.php');
-  }
+if (!empty($_SESSION['login'])) {
+
+    if ($_SESSION["rang"] == 0) {
+        header('Location: client.php');
+    }
+} else {
+    header('Location: login.php');
 }
-else{
-  header('Location: login.php');
-}
-include("include/head.php");
+include "include/head.php";
 ?>
 
 <body class="fixed-nav sticky-footer bg-dark" id="page-top">
       <!-- Example DataTables Card-->
 
-  <?php 
-    //Affichage du tableau des requetes click to call
-    echo"<div class='card mb-3'>
+  <?php
+//Affichage du tableau des requetes click to call
+echo "<div class='card mb-3'>
         <div class='card-header'>
         <h3 style='text-align:center; color:black;'>Tableau des Requêtes de Click to Call</h1>
         </div>
@@ -44,55 +43,55 @@ include("include/head.php");
               </thead>
               <tbody>";
 
-               //Requete PHP pour récupérer toute la table ctc_request
-              $requete='SELECT * FROM ctc_request WHERE etat=0 or etat=1' ;
-              $req = $bdd->query($requete);  
+//Requete PHP pour récupérer toute la table ctc_request
+$requete = 'SELECT * FROM ctc_request WHERE etat=0 or etat=1';
+$req = $bdd->query($requete);
 
-              // boucle pour afficher toutes les lignes de la table ctc_request
-			  $array_id = array();
-              while($row = $req->fetch()){ 
-              $user='SELECT * FROM users WHERE id="'.$row["user_id"].'"';
-              $userboard = $bdd->query($user);
-              $line = $userboard->fetch();
+// boucle pour afficher toutes les lignes de la table ctc_request
+$array_id = array();
+while ($row = $req->fetch()) {
+    $user = 'SELECT * FROM users WHERE id="' . $row["user_id"] . '"';
+    $userboard = $bdd->query($user);
+    $line = $userboard->fetch();
 
-           /*   $getId='SELECT * FROM ctc_request WHERE user_id="'.$line['id'].'"';
-              $id_client = $bdd->query($getId);
-              $id = $id_client->fetch();*/
-              ?>
+    /*   $getId='SELECT * FROM ctc_request WHERE user_id="'.$line['id'].'"';
+    $id_client = $bdd->query($getId);
+    $id = $id_client->fetch();*/
+    ?>
 
-              <td><?php echo $line['nom']?></td>
+              <td><?php echo $line['nom'] ?></td>
               <td><?php echo $line['prenom'] ?></td>
               <td><?php echo $line['mail'] ?></td>
               <td><?php echo $line['telephone'] ?></td>
               <td><?php echo $row['dates'] ?></td>
-              <td><?php if($row['etat']==0){
-                echo 'En attente';
-              } elseif($row['etat']==1){
-                echo 'En cours';
-              }else{
-                echo 'Terminé';}?></td> 
-				<td> 
+              <td><?php if ($row['etat'] == 0) {
+        echo 'En attente';
+    } elseif ($row['etat'] == 1) {
+        echo 'En cours';
+    } else {
+        echo 'Terminé';}?></td>
+				<td>
 				<form method="post" action="historique.php">
-				<input name="id" type="hidden" value="<?php echo $line['id']?>"></input>
+				<input name="id" type="hidden" value="<?php echo $line['id'] ?>"></input>
 				<button type="submit"><i class="fa fa-file" aria-hidden="true"></i></button>
 
 				</form>
-				</td>   
+				</td>
               </tr>
-              <?php } ?>
+              <?php }?>
               </tbody>
             </table>
           </div>
         </div>
-      </div> 
- 
-	
-      			 
+      </div>
+
+
+
     <!-- /.container-fluid-->
     <!-- /.content-wrapper-->
 
-<?php 
-include("include/foot.php");
+<?php
+include "include/foot.php";
 ?>
 
     <!-- Bootstrap core JavaScript-->
